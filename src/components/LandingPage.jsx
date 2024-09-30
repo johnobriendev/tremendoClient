307bc02a8896dcd2ca529b8c2bfc18b21ce85081
John O'Brien <johnobrien.dev@gmail.com>
2024-09-30 19:50:11 -0400

    update learn more button in landingPage

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -14,8 +14,8 @@ const LandingPage = () => {
     }}
     >
       <h1 className="text-4xl font-semibold mb-4 mt-24 xl:mt-12 2xl:mt-24">Tremendo</h1>
-      <p className="mb-6">Your go-to solution for everything you need.</p>
-      <Link to='/about' className='text-xl border rounded p-2 bg-slate-100 bg-opacity-50'>Learn More</Link>
+      <p className="mb-6">Your go-to solution for everything you need related to productivity and organization.</p>
+      <Link to='/about' className='text-xl border rounded px-4 py-2 bg-slate-100 bg-opacity-80'>Learn More</Link>
       <div className="flex space-x-4 mt-8">
         <Link to="/register">
           <button className="bg-blue-500 text-white px-4 py-2 rounded">Register</button>
